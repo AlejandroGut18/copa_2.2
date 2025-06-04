88b7e756c367608614d8ed079d4f892d1f95f4b8
AlejandroGut18 <egutierrezsequera2023@gmail.com>
2025-06-03 20:19:51 -0400

    Actualizo biblioteca

--- biblioteca.sql
+++ biblioteca.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-06-2025 a las 17:31:05
+-- Tiempo de generación: 04-06-2025 a las 02:14:07
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -96,7 +96,8 @@ INSERT INTO `detalle_permisos` (`id`, `id_usuario`, `id_permiso`) VALUES
 (84, 3, 4),
 (85, 3, 5),
 (86, 3, 8),
-(87, 3, 9);
+(87, 3, 9),
+(88, 1, 10);
 
 -- --------------------------------------------------------
 
@@ -222,7 +223,8 @@ INSERT INTO `permisos` (`id`, `nombre`, `tipo`) VALUES
 (6, 'Torneos', 6),
 (7, 'Materias', 7),
 (8, 'Reportes', 8),
-(9, 'Prestamos', 9);
+(9, 'ubicaciones', 9),
+(10, 'no se', 10);
 
 -- --------------------------------------------------------
 
@@ -272,26 +274,6 @@ INSERT INTO `status` (`id`, `nombre`, `descripcion`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `ubicaciones`
---
-
-CREATE TABLE `ubicaciones` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `direccion` varchar(260) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
-
---
--- Volcado de datos para la tabla `ubicaciones`
---
-
-INSERT INTO `ubicaciones` (`id`, `nombre`, `direccion`) VALUES
-(1, 'El Poligono', 'Redomas de Guaparo'),
-(2, 'Casa Portuguesa', 'San Diego');
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `torneos`
 --
 
@@ -311,10 +293,58 @@ CREATE TABLE `torneos` (
 INSERT INTO `torneos` (`id`, `nombre`, `fecha_inicio`, `fecha_fin`, `ubicacion_id`, `status_id`) VALUES
 (1, 'COPA 2025', '2023-07-01', '2023-07-20', 1, 1),
 (2, 'Super Copa 2025', '2025-06-01', NULL, 1, 1),
-(3, 'COPA 2026', '2026-01-17', '2025-05-31', 1, 1);
+(3, 'COPA 2026', '2026-01-17', '2025-05-31', 1, 1),
+(4, 'Copa Master', '2025-04-15', '2025-06-10', 1, 1),
+(5, 'Campeonato Final', '2025-05-06', '2025-06-25', 1, 1),
+(6, 'Campeonato 2024', '2024-03-04', '2024-10-23', 1, 1),
+(7, 'Torneo 2020', '2020-02-01', '2020-11-01', 1, 1),
+(8, 'Champion 2025', '2025-06-03', '2025-11-12', 1, 1);
+
+--
+-- Disparadores `torneos`
+--
+DELIMITER $$
+CREATE TRIGGER `grupos_por_defecto_torneo` AFTER INSERT ON `torneos` FOR EACH ROW BEGIN
+  -- Grupos masculinos
+  INSERT INTO grupos (nombre, status_id, torneo_id, genero) VALUES
+  ('A', 1, NEW.id, 'M'),
+  ('B', 1, NEW.id, 'M'),
+  ('C', 1, NEW.id, 'M'),
+  ('D', 1, NEW.id, 'M');
+
+  -- Grupos femeninos
+  INSERT INTO grupos (nombre, status_id, torneo_id, genero) VALUES
+  ('A', 1, NEW.id, 'F'),
+  ('B', 1, NEW.id, 'F'),
+  ('C', 1, NEW.id, 'F'),
+  ('D', 1, NEW.id, 'F');
+END
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
+--
+-- Estructura de tabla para la tabla `ubicaciones`
+--
+
+CREATE TABLE `ubicaciones` (
+  `id` int(10) UNSIGNED NOT NULL,
+  `nombre` varchar(100) NOT NULL,
+  `direccion` varchar(260) NOT NULL,
+  `status_id` int(10) UNSIGNED NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `ubicaciones`
+--
+
+INSERT INTO `ubicaciones` (`id`, `nombre`, `direccion`, `status_id`) VALUES
+(1, 'El Poligono', 'Redomas de Guaparo', 1),
+(2, 'Casa Portuguesa', 'San Diego', 1),
+(3, 'Draculandia', 'Parque Recreacional del Sur', 1);
+
+-- --------------------------------------------------------
 
 --
 -- Estructura de tabla para la tabla `usuarios`
@@ -333,10 +363,11 @@ CREATE TABLE `usuarios` (
 --
 
 INSERT INTO `usuarios` (`id`, `usuario`, `nombre`, `clave`, `estado`) VALUES
-(1, 'admin', 'Super Administrador', '8f00793014a5cd4994a0cbb999dbba67f9a135d5184ca326d082a65a8ad2d849', 1),
+(1, 'admin', 'Super Admin', '8f00793014a5cd4994a0cbb999dbba67f9a135d5184ca326d082a65a8ad2d849', 1),
 (2, 'angel', 'Vida Informatico', '519ba91a5a5b4afb9dc66f8805ce8c442b6576316c19c6896af2fa9bda6aff71', 1),
 (3, 'alex', 'Alejandro Gutierrez', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 1),
-(4, 'Laura Lara', 'Lou', '8f00793014a5cd4994a0cbb999dbba67f9a135d5184ca326d082a65a8ad2d849', 1);
+(4, 'Laura Lara', 'Lou', '8f00793014a5cd4994a0cbb999dbba67f9a135d5184ca326d082a65a8ad2d849', 1),
+(5, 'delegado', 'Delegado', '8f00793014a5cd4994a0cbb999dbba67f9a135d5184ca326d082a65a8ad2d849', 1);
 
 --
 -- Índices para tablas volcadas
@@ -421,7 +452,8 @@ ALTER TABLE `torneos`
 -- Indices de la tabla `ubicaciones`
 --
 ALTER TABLE `ubicaciones`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `fk_ubicaciones_status` (`status_id`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -449,7 +481,7 @@ ALTER TABLE `configuracion`
 -- AUTO_INCREMENT de la tabla `detalle_permisos`
 --
 ALTER TABLE `detalle_permisos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=88;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=89;
 
 --
 -- AUTO_INCREMENT de la tabla `editorial`
@@ -479,7 +511,7 @@ ALTER TABLE `materia`
 -- AUTO_INCREMENT de la tabla `permisos`
 --
 ALTER TABLE `permisos`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT de la tabla `prestamo`
@@ -497,19 +529,19 @@ ALTER TABLE `status`
 -- AUTO_INCREMENT de la tabla `torneos`
 --
 ALTER TABLE `torneos`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT de la tabla `ubicaciones`
 --
 ALTER TABLE `ubicaciones`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Restricciones para tablas volcadas
@@ -543,6 +575,12 @@ ALTER TABLE `prestamo`
 ALTER TABLE `torneos`
   ADD CONSTRAINT `fk_torneo_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `fk_torneo_ubicacion` FOREIGN KEY (`ubicacion_id`) REFERENCES `ubicaciones` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `ubicaciones`
+--
+ALTER TABLE `ubicaciones`
+  ADD CONSTRAINT `fk_ubicaciones_status` FOREIGN KEY (`status_id`) REFERENCES `status` (`id`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
